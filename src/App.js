86b1cf5fe6565657f777bad9678a0abf720770fb
amitr3647 <amitr3647@gmail.com>
2021-09-30 08:32:26 +0530

    saving to the local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Item from "./Components/Item/Item";
 import "./App.css";
 import Input from "./Components/Input/Input";
 
+const getLocalItems = () => {
+  let localItem = localStorage.getItem("addedItem");
+  if (localItem) {
+    return JSON.parse(localStorage.getItem("addedItem"));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalItems());
+  // localStorage.setItem("addedItem", [...items, items]);
 
+  // console.log("updatedItems", updatedItems);
+  useEffect(() => {
+    // console.log(items);
+    localStorage.setItem("addedItem", JSON.stringify(items));
+  }, [items]);
   return (
     <div className="App">
       <Input
